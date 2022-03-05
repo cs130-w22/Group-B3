4ffaf142d5bbc9063de3a4a88f5768efdd336989
ble720 <ble720@g.ucla.edu>
2022-03-05 02:13:33 -0800

    Added region of convergence and process overlap to Pipeline and get cropped images function to MLmodel

--- Backend/api/MLmodel.py
+++ Backend/api/MLmodel.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import cv2
 
 def get_crops(image, boxes):
     crops = []
--- Backend/api/Pipeline.py
+++ Backend/api/Pipeline.py
@@ -35,8 +35,40 @@ class Pipeline:
             crops = get_crops(img)
             img.set_crop(crops)
 
+    def regionOfConvergence(self, box1, box2):
+        x1 = max(box1[0], box2[0])
+        x2 = min(box1[2], box2[2])
+        y1 = max(box1[1], box2[1])
+        y2 = min(box1[3], box2[3])
 
-        
+        if x1 >= x2 or y1 >= y2:
+            return [0,0]
+
+        conv_area = (x2-x1)*(y2-y1)
+        box1_area = (box1[2]-box1[0])*(box1[3]-box1[1])
+        box2_area = (box2[2]-box2[0])*(box2[3]-box2[1])
+
+        return [conv_area/box1_area, conv_area/box2_area]
+
+
+    def processOverlap(self, img_boxes):
+        i = 0
+        while i < len(img_boxes):
+            j = i+1
+            while j < len(img_boxes):
+                roc = self.regionOfConvergence(img_boxes[i][0], img_boxes[j][0])
+
+                if roc[0] >= 0.9 or roc[1] >= 0.9:
+                    if img_boxes[i][1] >= img_boxes[j][1]: #if confidence of box i > box j
+                        img_boxes.remove(img_boxes[j])   
+                    else:
+                        img_boxes.remove(img_boxes[i])
+                        j = i+1 
+                else:
+                    j += 1
+            i += 1            
+
+        return img_boxes
 
     def zip_crops(self):
         return self.io.create_zip(self.image_info_struct)

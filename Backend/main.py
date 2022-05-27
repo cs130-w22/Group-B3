347edaaba1261acfdef394c940c550e08543b13e
ble720 <ble720@g.ucla.edu>
2022-05-27 03:18:43 -0700

    Updated main.py to delete all input images within folders

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,14 @@
 from Pipeline import Pipeline
+import os
 
 if __name__ == "__main__":
     parseArgs = None
 
     pipe = Pipeline()
     pipe.process_all()
+
+    os.remove('./input_images/input.zip')
+
+    front_end_dir = './../NodejsWebApp1/NodejsWebApp1/Images'
+    for file in os.listdir(front_end_dir):
+        os.remove(os.path.join(front_end_dir, file))

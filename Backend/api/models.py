c065f64defec7c8586c4ca11d232cf86c5946eb0
ble720 <ble720@g.ucla.edu>
2022-05-27 03:13:51 -0700

    delete unnecesary stuff

--- Backend/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# class ImageZip(models.Model):
-#     name = models.CharField(max_length=40, default="", unique=True)
-#     image_bytes = None
--- Backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import handle_post
-
-urlpatterns = [
-    path('', handle_post)
-]
